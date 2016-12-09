76acfdd671d794a5f041da6ca89a99f5b5a52145
TestCtx <test.ctx@outlook.com>
2016-12-09 16:08:41 +0000

    Create test.js

---
+++ test.js
@@ -0,0 +1,18 @@
+---
+---
+JS
+(function() {
+  var list, math, square;
+  square = function(x) {
+    return x * x;
+  };
+  list = [1, 2, 3, 4, 5];
+  math = {
+    root: Math.sqrt,
+    square: square,
+    cube: function(x) {
+      return x * square(x);
+    }
+  };
+}).call(this);
+JS
